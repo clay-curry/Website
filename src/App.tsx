1e552bec49145ce00bb51f4467255f437fcfb706
Clay Curry (Home) <claycurry34@gmail.com>
2022-01-24 18:58:29 -0600

    created introductory message

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Welcome To My Website
+        </p>        
+        <p>
           Clayton Curry
         </p>
       </header>

5727d1a5022ff0d322782663298b7d43b9828ada
Clay Curry <claycurry34@gmail.com>
2022-01-29 00:39:23 -0600

    implemented appropriate styling for the home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,29 +5,34 @@ class TitleBar extends React.Component {
     state = { }
     render() { 
         return ( 
-            <div className='bar'>
-                <h1 className='author-name'>Clayton Curry</h1>
+            <div className='nav'>
+                <div className='nav-item author-name'>
+                    <h1><a href="/">Clayton Curry</a></h1>
+                </div>
 
-                <img className='profile-pic' src={require('../images/Clay_Face.JPG')} alt="me" />
+                <div className='nav-item'>
+                    <div className=' profile-pic'><img className='img' src={require('../images/Clay_Face.JPG')} alt="me" /></div>
+                </div>
                 
-                <div className='button-container'>
-                    <a className='flex-button' target='_self' href='/'>
-                        <div> 
+                
+                <div className=' nav-item buttons-container'>
+                    <a target='_self' href='/'>
+                        <div className='button'>
                         Research
                         </div>
                     </a>
-                    <a className='flex-button' target='_blank' rel={'noreferrer'} href='https://www.jezzamon.com/fourier/index.html'>
-                        <div> 
+                    <a target='_blank' rel={'noreferrer'} href='https://www.jezzamon.com/fourier/index.html'>
+                        <div className='button'> 
                         Interesting Stuff
                         </div>
                     </a>
-                    <a className='flex-button' target='_self' href='/'>
-                        <div> 
+                    <a target='_self' href='/'>
+                        <div className='button'> 
                         Projects
                         </div>
                     </a>
-
                 </div>
+
             </div>
          );
     }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,27 +6,29 @@ function Home() {
   return (
     <>
       <NavBar /> {/* Component Class Import */}
-      <Body />
+      <Body>
+      <AboutMe />
+      <GenericSection title="Research Statement">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </GenericSection>
+      <GenericSection title="Past Work"> 
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      </GenericSection>
+      <References />
+      </Body>
     </>
   );
 }
 
-function Body() {
-  return (
-    <div className={ 'home-body' }>
-      <AboutMe />
-      <GenericSection title="Research Statement" />
-      <GenericSection title="Past Work" />
-      <References />
-    </div>
-  );
+function Body(props: {children: any }) {
+  return <div className={ 'body' }>{props.children}</div>
 }
 
 function AboutMe()
 {
   return (<>
   <h1>About Me</h1>
-  <p><b>I am a</b> final year computer science and mathematics student at the University of Oklahoma.</p>
+  <p><b>I am a</b> final year computer science and mathematics undergraduate student at the University of Oklahoma.</p>
   </>);
 }
 
@@ -54,11 +56,11 @@ function References()
   );
 }
 
-function GenericSection(props: { title: string; body?: string })
+function GenericSection(props: { title: string, children: any })
 {
   return (<div>
   <h2>{props.title}</h2>
-  <p>{props.body}</p>
+  <p>{props.children}</p>
   </div>);
 }
 

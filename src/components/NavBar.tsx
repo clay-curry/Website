4603b0360d5ab5b79bac9366e4997c25a288acac
Clay Curry <claycurry34@ou.edu>
2022-04-05 19:22:49 -0500

    updated navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,39 +2,41 @@ import React from 'react';
 import '../styles/navbar.css';
 
 class TitleBar extends React.Component {
-    state = { }
-    render() { 
-        return ( 
+    state = {}
+    render() {
+        return (
             <div className='nav'>
-                <div className='nav-item author-name'>
+
+                <div className='author-name'>
                     <h1><a href="/">Clayton Curry</a></h1>
                 </div>
 
-                <div className='nav-item'>
-                    <div className=' profile-pic'><img className='img' src={require('../images/Clay_Face.JPG')} alt="me" /></div>
+
+                <div className='profile-pic'>
+                    <img src={require('../images/Clay_Face.JPG')} alt="Portrait of Clayton Curry" />
                 </div>
-                
-                
-                <div className=' nav-item buttons-container'>
+
+
+                <div className='buttons-container'>
                     <a target='_self' href='/'>
                         <div className='button'>
-                        Research
+                            Research
                         </div>
                     </a>
                     <a target='_blank' rel={'noreferrer'} href='https://www.jezzamon.com/fourier/index.html'>
-                        <div className='button'> 
-                        Interesting Stuff
+                        <div className='button'>
+                            Interesting Stuff
                         </div>
                     </a>
                     <a target='_self' href='/'>
-                        <div className='button'> 
-                        Projects
+                        <div className='button'>
+                            Projects
                         </div>
                     </a>
                 </div>
 
             </div>
-         );
+        );
     }
 }
 

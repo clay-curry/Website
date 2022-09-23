f78c3dc6d1da6db4587686d39680e3c0e8ff0820
Clayton Curry <claycurry34@ou.edu>
2022-09-23 01:35:01 -0500

    adding fusion subdirectory

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -22,18 +22,27 @@ function Work() {
   const exch="https://globe.adsbexchange.com/"
   const oadii="https://www.ou.edu/oadii"
   const adsb = "https://globe.adsbexchange.com/"
+  const osn = "https://opensky-network.org/"
   return (<Fragment>
       <hr />
       <h3>Work Experience</h3>
       <ul className='experience-item'>
         <tr>Undergraduate Research Assistant with <a href={oadii}>SWEG-76</a></tr>
         <tr>Dec 2021 - Present</tr>
-        <tr><p>Early on, I implemented a set of data-scraping tools for aggregating transponder (<a href={adsb}>ADS-B</a>) messages on a global 
-            scale. My tool was able to intercept more than 10,000,000 messages within 48 hours. Achieving this feat required nothing more than
-            becoming a "human transpiler," for client-facing Javascript code at <a href={exch}>ADSB Exchange</a>, which I converted into equivalent
-            Python requests and SQL transactions.
-            </p><p>More recently, I have been conducting reviews of primary and secondary sources in the object tracking literature. My latest work is 
-              geared toward single object tracking in clutter and under random markovian jumps (where object dynamics rapidly and unexpectedly change).</p></tr>
+        <tr>
+          <p>
+            Early on, I implemented a set of web-scraping tools for aggregating citizen-sourced transponder messages (<a href={adsb}>ADS-B</a>) on a global 
+            scale, intercepting more than 10,000,000 (ten-million) messages served by <a href={exch}>ADSB Exchange</a> within 72 hours, which I saved to my own 
+            SQL-like database. Achieving this feat required nothing more than translating client-facing Javascript code into equivalent Python requests and SQL 
+            transactions. I later realized that researchers at <a href={osn}>OpenSkyNet</a> have been collecting, storing, and sharing this type of data (and
+            more) for years, thus my efforts resulted in nothing more than a learning lesson.
+            </p>
+            <p>
+            More recently, I have been getting my feet wet in primary and secondary literature in the decades-old science of multiple object tracking. On a 
+            simultaneous front, I have been adapting open-source software projects, including state-of-the-art tracking algorithms such as the Gaussian-Mixture 
+            Probability Hypothesis Density filter.
+            </p>
+        </tr>
       </ul>
   </Fragment>);
 }

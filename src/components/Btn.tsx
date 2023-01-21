8aaa4a9fb484a38053148c7914234c85c466d08e
Clay Curry <claycurry34@gmail.com>
2023-01-21 05:39:02 -0600

    AfeA

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -3,9 +3,9 @@ import '../styles/buttons.css'
 
 function Btn(props: { rel: string, href: string, className: string, text: string; }) {
     return (
-        <button rel={props.rel} href={props.href} className={props.className}>
+        <a href={props.href} className={props.className}>
             <span>{props.text}</span>
-        </button>
+        </a>
     );
 }
 
